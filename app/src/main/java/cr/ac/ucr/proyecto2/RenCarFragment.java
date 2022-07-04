360c0fff8e94a2de8c018d249e7d8de5c0e7d65b
maikelmatamoros <37597397+maikelmatamoros@users.noreply.github.com>
2022-07-04 14:49:21 -0600

    Initial commit

---
+++ app/src/main/java/cr/ac/ucr/proyecto2/RenCarFragment.java
@@ -0,0 +1,64 @@
+package cr.ac.ucr.proyecto2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RenCarFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RenCarFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public RenCarFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment RenCarFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static RenCarFragment newInstance(String param1, String param2) {
+        RenCarFragment fragment = new RenCarFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ren_car, container, false);
+    }
+}
